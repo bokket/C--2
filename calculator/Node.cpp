8f1612f9aed2647ac4b5b436b0ff01f92039363c
bokket <3100563328@qq.com>
2020-11-22 02:23:36 +0800

    commit

---
+++ calculator/Node.cpp
@@ -0,0 +1,47 @@
+//
+// Created by bokket on 2020/11/22.
+//
+
+#include "Node.h"
+#include <cmath>
+#include <iostream>
+double NumberNode::Calu() const
+{
+    return number_;
+}
+
+BinaryNode::~BinaryNode()
+{
+    delete right_;
+    delete left_;
+}
+
+
+double AddNode::Calc() const
+{
+    return left_->Calc()+right_->Calc();
+}
+double SubNode::Calc() const
+{
+    return left_->Calc()-right_->Calc();
+}
+double MultiplyNode::Calc() const
+{
+    return left_->Calc()*right_->Calc();
+}
+double DivideNode::Calc() const
+{
+    double divsion=right_->Calc();
+    if(divsion!=0.0)
+            return left_->Calc()/divsion;
+    else
+    {
+        cout<<"Error Divison by zero"<<endl;
+        return HUGE_VAL;
+    }
+}
+
+double UMinusNode::Calu() const
+{
+    return - ( child_->Calc() );
+}
---
+++ calculator/Node.h
@@ -0,0 +1,75 @@
+//
+// Created by bokket on 2020/11/22.
+//
+
+#ifndef CALCULATOR_NODE_H
+#define CALCULATOR_NODE_H
+
+
+class Node {
+public:
+    virtual double Calc() const =0;
+    virtual ~Node();
+};
+class NumberNode : public Node
+{
+public:
+    NumberNode(double number):number_(number){}
+    double Calu() const;
+
+private:
+    const double number_;
+};
+class UnaryNode: public Node
+{
+public:
+    UnaryNode(Node* child):child_(child){}
+    ~UnaryNode();
+protected:
+    Node* const child_;
+};
+class BinaryNode: public Node
+{
+public:
+    BinaryNode(Node* left,Node* right):left_(left),right_(right){}
+    ~BinaryNode();
+protected:
+    Node* const left_;
+    Node* const right_;
+};
+class AddNode: public BinaryNode
+{
+public:
+    AddNode(Node* left,Node* right):BinaryNode(left,right){}
+
+    double Calc() const;
+};
+class SubNode: public BinaryNode
+{
+public:
+    SubNode(Node* left,Node* right):BinaryNode(left,right){}
+
+    double Calc() const;
+};
+class MultiplyNode: public BinaryNode
+{
+public:
+    MultiplyNode(Node* left,Node* right):BinaryNode(left,right){}
+
+    double Calc() const;
+};
+class DivideNode: public BinaryNode
+{
+public:
+    DivideNode(Node* left,Node* right):BinaryNode(left,right){}
+
+    double Calc() const;
+};
+
+class UMinusNode: public UnaryNode
+{
+public:
+    UMinusNode(Node* child):UnaryNode(child){}
+    double Calu() const;
+};
+#endif //CALCULATOR_NODE_H
